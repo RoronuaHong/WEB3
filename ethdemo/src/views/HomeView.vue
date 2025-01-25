93758c93663ec4e1e304e1691170f3f9a3a9d70d
RoronuaHong <807672412@qq.com>
2025-01-26 04:11:53 +0800

    update

--- ethdemo/src/views/HomeView.vue
+++ ethdemo/src/views/HomeView.vue
@@ -4,9 +4,11 @@
     <van-button type="primary">创建钱包</van-button>
     <Web3Api></Web3Api>
     <div>余额: {{ amount }}</div>
+    <h1>转账: </h1>
+    <van-divider/>
+    <van-button type="primary" @click="send">获取费用</van-button>
   </div>
 </template>
-
 <script setup>
 import { ref } from "vue";
 import Web3 from "web3";
@@ -39,4 +41,24 @@ const num1 = web3.utils.toWei(0.00003, "ether");
 const num2 = web3.utils.fromWei(30000, "ether");
 console.log(4, "num1: ", num1, web3.utils.toWei);
 console.log(5, "num2: ", num2);
+
+// eth转账
+const send = async() => {
+  // 创建转账参数
+  // 获取账户交易次数
+  const nonce = await web3.eth.getTransactionCount(account.address);
+
+  console.log(nonce);
+
+  // 获取预计转账 gas费用
+  const gasPrice = await web3.eth.getGasPrice();
+
+  console.log('gasPrice: ', gasPrice);
+
+  // 转账金额: 以 wei 作为单位
+  const value = web3.utils.toWei('0.01');
+}
+
+// web3.eth.sendSignedTransaction();
+
 </script>
